9c766b85cf051a2fd298fbbdaef86ed5eccaf255
Kimsehwan96 <sktpghks138@gmail.com>
2021-01-05 01:12:57 +0900

    add board list view & board detail view

--- community/board/urls.py
+++ community/board/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('detail/<int:pk>/', views.board_detail), #숫자형인 변수명 Pk인 놈을 받겠다. 뷰에 전달됨
     path('list/', views.board_list),
     path('write/', views.board_write),
 ]
--- community/board/views.py
+++ community/board/views.py
@@ -1,13 +1,36 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Board
 from .forms import BoardForm
+from user.models import User
 # Create your views here.
 
+def board_detail(request, pk):
+    board = Board.objects.get(pk=pk) #디테일에 해당하는 글
+    context = {
+        'board' : board
+    }
+    return render(request, 'board_detail.html', context)
+
 def board_write(request):
+    if request.method == 'POST':
+        form = BoardForm(request.POST)
+        if form.is_valid():
+            user_id = request.session.get('user')
+            user = User.objects.get(pk=user_id)
+            board = Board()
+            board.title = form.cleaned_data['title'] #cleaned_data는 BoardForm 클래스가 상속받은 베이스 클래스에 있다.
+            board.contents = form.cleaned_data['contents']
+            board.writer = user
+            board.save()
+
+            return redirect('/board/list')
+    else:
+        form = BoardForm()
+
     form = BoardForm()
     context = {'form' : form}
     return render(request, 'board_write.html', context) #이렇게 context라고 명시해서 넘기는게 편한듯
-    
+
 
 def board_list(request):
     #클래스.objects().all() <- 모든것을 가져옴 
