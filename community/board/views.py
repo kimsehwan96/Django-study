d31062fbe75bdc4da4d5f97b576fc85892106c8b
Kimsehwan96 <sktpghks138@gmail.com>
2021-01-06 01:47:58 +0900

    add pagination & add logic for error handling

--- community/board/views.py
+++ community/board/views.py
@@ -1,17 +1,26 @@
+from django.core import paginator
 from django.shortcuts import render, redirect
+from django.core.paginator import Paginator
+from django.http import Http404
 from .models import Board
 from .forms import BoardForm
 from user.models import User
 # Create your views here.
 
 def board_detail(request, pk):
-    board = Board.objects.get(pk=pk) #디테일에 해당하는 글
+    try:
+        board = Board.objects.get(pk=pk) #디테일에 해당하는 글
+    except Board.DoesNotExist:
+        raise Http404('게시글을 찾을 수 없습니다.')
     context = {
         'board' : board
     }
     return render(request, 'board_detail.html', context)
 
 def board_write(request):
+    if not request.session.get('user'): #세션 정보에 User 가 없으면
+        return redirect('/user/login') #login 페이지로 리다이렉트
+    
     if request.method == 'POST':
         form = BoardForm(request.POST)
         if form.is_valid():
@@ -34,6 +43,13 @@ def board_write(request):
 
 def board_list(request):
     #클래스.objects().all() <- 모든것을 가져옴 
-    boards = Board.objects.all().order_by('-id') #-id는 게시글 id의 역순이라는 의미
-    
-    return render(request, 'board_list.html', {'boards' : boards })
+    all_boards = Board.objects.all().order_by('-id') #-id는 게시글 id의 역순이라는 의미
+    page = request.GET.get('p', 1)
+    paginator = Paginator(all_boards, 2) #페이지네이터 클래스 인스턴스 생성
+
+    boards = paginator.get_page(page) #페이지네이터 인스턴스의 메서드중 get_page 사용
+
+    context = {
+        'boards' : boards
+    }
+    return render(request, 'board_list.html', context)
