7c3a825358f5fd06b75c891eb6ba6e69dc0002da
Kimsehwan96 <sktpghks138@gmail.com>
2021-01-10 18:44:47 +0900

    [add] add transaction & atomic logic for managin stock of products

--- shopping/order/forms.py
+++ shopping/order/forms.py
@@ -2,6 +2,7 @@ from django import forms
 from .models import Order
 from product.models import Product
 from user.models import User
+from django.db import transaction #트랜잭션을 위해서
 
 class RegisterForm(forms.Form):
 
@@ -31,13 +32,20 @@ class RegisterForm(forms.Form):
         
 
         if quantity and product and user:
-            order = Order(
-                quantity=quantity,
-                product=Product.objects.get(pk=product),
-                user=User.objects.get(email=user)
-            )
-
-            order.save() #실제 이러한 주문을 저장하기.
+            with transaction.atomic(): #원자적
+                prod = Product.objects.get(pk=product)
+                order = Order(
+                    quantity=quantity,
+                    product=prod,
+                    user=User.objects.get(email=user)
+                )
+                order.save() #실제 이러한 주문을 저장하기.
+                prod.stock -= quantity #재고를 quantity만큼 제거.
+                prod.save() #주문정보 저장 이후 product의 재고 정보도 저장
+            
+            #transaction.atomic()안에서 실행할경우 원자적으로 차리됨.
+            #실제 테스트 결과 원자적 수행 잘 된다.
+            # ->transaction & atomic 연산에 대해서 좀 더 알아보기 !!
         else:
             self.product = product
             self.add_error('quantity', '값이 없습니다.')
