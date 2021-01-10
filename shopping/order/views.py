538708bae70f4f6eda15f725a03bcc250869962c
Kimsehwan96 <sktpghks138@gmail.com>
2021-01-10 19:15:20 +0900

    [add][modify] add order list view logic & temlates & modify some url & view

--- shopping/order/views.py
+++ shopping/order/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import redirect, render
 from django.views.generic.edit import FormView
+from django.views.generic import ListView
 from .forms import RegisterForm
+from .models import Order
 # Create your views here.
 
 class OrderCreate(FormView):
@@ -20,3 +22,20 @@ class OrderCreate(FormView):
 
     ##부모 클래스의 get_form_kwargs를 호출하여 kw에 저장하고
     ## kw를 업데이트하는데 -> request를 넣어준다. (사용해되니까)
+
+
+class OrderList(ListView):
+    # model = Order <- 모든 유저의 정보를 얻는 코드
+    template_name = 'order.html'
+    context_object_name = 'order_list'
+    #TODO: 현재는 모든 유저의 주문 정보가 나온다
+    #로그인한 사용자의 주문만 보기위해서는 queryset 을 이용해야하며
+    #또 세션을 사용해야 한다.
+    
+    def get_queryset(self, **kwargs):
+        queryset = Order.objects.filter(user__email=self.request.session.get('user'))
+        return queryset
+
+        #이렇게 하면 로그인한 사용자의 주문 정보만 나온다
+        #object.filter 내부의 user__email <- 이 구문이 어떻게 나온건지 좀 알아봐야것다
+        
