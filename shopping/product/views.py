5e40ecd2152c0c1bf00e6e14e92dcb61846668a8
Kimsehwan96 <sktpghks138@gmail.com>
2021-01-10 17:24:15 +0900

    [add] add product's detail view page & convert raw-tage into real html tage with django filter

--- shopping/product/views.py
+++ shopping/product/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from django.views.generic.edit import FormView
 from .models import Product
 from .forms import RegisterForm
@@ -16,3 +16,10 @@ class ProductCreate(FormView):
     template_name = 'register_product.html'
     form_class = RegisterForm
     success_url = '/product/'
+
+class ProductDetail(DetailView):
+    template_name = 'product_detail.html'
+    queryset = Product.objects.all()#쿼리셋 정의 가능
+    #queryset = Product.objects <- 이부분에 필터를 걸 수 있다.
+    context_object_name = 'product' #컨텍스트명 명시하기
+    
